8d67238840b9c6e8d4680f36eff05a9fc94f55be
Hunter Ritter <trey353@gmail.com>
2018-04-19 20:01:58 -0400

    Add existing files

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const gravatar = require("gravatar");
+//const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
 
 // Load User model
@@ -21,15 +21,15 @@ router.post("/register", (req, res) => {
     if (user) {
       return res.status(400).json({ email: "Email already exists" });
     } else {
-      const avatar = gravatar.url(req.body.email, {
-        s: "200", //Size
-        r: "pg", //Rating
-        d: "mm" //Default
-      });
+      // const avatar = gravatar.url(req.body.email, {
+      //   s: "200", //Size
+      //   r: "pg", //Rating
+      //   d: "mm" //Default
+      // });
       const newUser = new User({
         name: req.body.name,
         email: req.body.email,
-        avatar,
+        // avatar,
         password: req.body.password
       });
       bcrypt.genSalt(10, (err, salt) => {
